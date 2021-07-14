bc619ca7e9c3f4b67a5445fae72668c8f8948c1e
Mark Wilson-Thomas <mwthomas@microsoft.com>
2021-07-14 15:23:35 -0700

    Changes from B

--- RestGetIndicesAction.cs
+++ RestGetIndicesAction.cs
@@ -3,8 +3,9 @@ public class RestGetIndicesAction : BaseRestHandler {
     private readonly IndexScopedSettings indexScopedSettings;
     
     [Inject()]
-    public RestGetIndicesAction(Settings settings, RestController controller, Client client) 
+    public RestGetIndicesAction(Settings settings, RestController controller, Client client, IndexScopedSettings indexScopedSettings) 
             : base(settings, controller, client) {
+        this.indexScopedSettings = indexScopedSettings;
         controller.registerHandler(GET, "/{index}", this);
         controller.registerHandler(GET, "/{index}/{type}", this);
     }
